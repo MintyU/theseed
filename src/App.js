67472c7b09e0784df0a190fb489e9881a9a7a890
MintyU <deathcat@outlook.kr>
2020-08-29 01:23:30 +0900

    #Send F0 data to App.js

--- src/App.js
+++ src/App.js
@@ -6,23 +6,31 @@ import F1 from "./floors/f1";
 
 function App() {
   const [floor, setFloor] = useState(0);
+  const [f0Data, setF0Data] = useState(0);
+
+  const handleF0 = (f0Input) => {
+    setF0Data(f0Input);
+  };
+
   return (
     <div className="App">
-      {floor === 0 && <F0 />}
+      {floor === 0 && <F0 f0Callback={handleF0} />}
       {floor === 1 && <F1 />}
+      <div>
+        <button
+          className="prev"
+          onClick={() => floor === 0 || setFloor(floor - 1)}
+        >
+          이전
+        </button>
 
-      <button
-        className="prev"
-        onClick={() => floor === 0 || setFloor(floor - 1)}
-      >
-        이전
-      </button>
-
-      <button className="next" onClick={() => setFloor(floor + 1)}>
-        다음
-      </button>
-
-      {floor}
+        <button className="next" onClick={() => setFloor(floor + 1)}>
+          다음
+        </button>
+      </div>
+      floor: {floor}
+      <br />
+      f0Data: {f0Data}
     </div>
   );
 }
--- src/floors/f0.js
+++ src/floors/f0.js
@@ -1,13 +1,15 @@
 import React from "react";
 
-class F0 extends React.Component {
-  render() {
-    return (
-      <div id="0">
-        <h1>Lobby</h1>
-      </div>
-    );
-  }
+function F0({ f0Callback }) {
+  return (
+    <div id="0">
+      <h1>로비</h1>
+      <p>시작하기 전 가지고 있던 시드 포인트를 입력해주세요.</p>
+      <span>
+        <input type="text" onChange={(e) => f0Callback(e.target.value)}></input>
+      </span>
+    </div>
+  );
 }
 
 export default F0;
