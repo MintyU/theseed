6087a2a45c788fd590888559df264ccd2bfb89ef
MintyU <deathcat@outlook.kr>
2020-08-28 15:49:54 +0900

    Updates

---
+++ precache-manifest.3bbc1b6dbd8d4ae9f70549453082132b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cedb116542d2e132bc97f796fc9ba363",
+    "url": "/theseed/index.html"
+  },
+  {
+    "revision": "76d9811314ca373e8e4c",
+    "url": "/theseed/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "d27040d0c2b434c36a84",
+    "url": "/theseed/static/js/2.2ae52035.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/theseed/static/js/2.2ae52035.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "76d9811314ca373e8e4c",
+    "url": "/theseed/static/js/main.15f1f698.chunk.js"
+  },
+  {
+    "revision": "44a6197681b065a813f4",
+    "url": "/theseed/static/js/runtime-main.3b07874b.js"
+  }
+]);
