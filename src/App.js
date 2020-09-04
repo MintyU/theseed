3bb94eb364c3385ea8b9bfbaa6f2075879d837e7
MintyU <deathcat@outlook.kr>
2020-09-04 15:33:32 +0900

    #F2 Done

--- src/App.js
+++ src/App.js
@@ -15,20 +15,7 @@ function App() {
   const [f2Total, setF2Total] = useState(0);
   const [f2Cards, setF2Cards] = useState([0, 0, 0, 0]);
 
-  const handleF0 = (f0Input) => {
-    setF0Data(f0Input);
-  };
-
-  const handleF1_0 = (f1Input0) => {
-    setF1Data0(f1Input0);
-  };
-  const handleF1_1 = (f1Input1) => {
-    setF1Data1(f1Input1);
-  };
-
-  {
-    /*Change F2 First Card's Color*/
-  }
+  /*Change F2 First Card's Color*/
   const handleF2First0 = (colorInput) => {
     setF2First(
       f2First.map((a, i) => {
@@ -41,9 +28,7 @@ function App() {
     );
   };
 
-  {
-    /*Change F2 First Card's Number*/
-  }
+  /*Change F2 First Card's Number*/
   const handleF2First1 = (numInput) => {
     setF2First(
       f2First.map((a, i) => {
@@ -56,6 +41,34 @@ function App() {
     );
   };
 
+  /*Change F2 Last Card's Color*/
+  const handleF2Last0 = (colorInput) => {
+    setF2Last(
+      f2Last.map((a, i) => {
+        if (i === 0) {
+          return colorInput;
+        } else {
+          return a;
+        }
+      })
+    );
+  };
+
+  /*Change F2 Last Card's Number*/
+  const handleF2Last1 = (numInput) => {
+    setF2Last(
+      f2Last.map((a, i) => {
+        if (i === 1) {
+          return numInput;
+        } else {
+          return a;
+        }
+      })
+    );
+  };
+
+  /* cardNum is the number of the Card!(number 8 is the violet card)
+    argument "i" is the color of the card(0:red, 1:blue, 2:green, 3: yellow)*/
   const handleF2Cards = (cardNum, i) => {
     setF2Cards(
       f2Cards.map((card, j) => {
@@ -70,17 +83,24 @@ function App() {
 
   return (
     <div className="App">
-      {floor === 0 && <F0 f0Data={f0Data} f0Callback={handleF0} />}
+      {floor === 0 && <F0 f0Data={f0Data} f0Callback={setF0Data} />}
       {floor === 1 && (
         <F1
           f1Data0={f1Data0}
           f1Data1={f1Data1}
-          f1Callback0={handleF1_0}
-          f1Callback1={handleF1_1}
+          f1Callback0={setF1Data0}
+          f1Callback1={setF1Data1}
         />
       )}
       {floor === 2 && (
-        <F2 f2Callback0={handleF2First0} f2Callback1={handleF2First1} />
+        <F2
+          f2Callback0={handleF2First0}
+          f2Callback1={handleF2First1}
+          f2Callback2={handleF2Cards}
+          f2Callback3={handleF2Last0}
+          f2Callback4={handleF2Last1}
+          f2Callback5={setF2Total}
+        />
       )}
       <div>
         <button
@@ -107,10 +127,17 @@ function App() {
         <li key={i}>{a}</li>
       ))}
       <br />
+      f2Last:
+      {f2Last.map((a, i) => (
+        <li key={i}>{a}</li>
+      ))}
+      <br />
       f2Cards:
       {f2Cards.map((card, idx) => (
         <li key={idx}>{card}</li>
       ))}
+      f2Total:
+      {f2Total}
     </div>
   );
 }
--- src/floors/f2.js
+++ src/floors/f2.js
@@ -1,11 +1,18 @@
 import React from "react";
 
-function F2({ f2Callback0, f2Callback1 }) {
+function F2({
+  f2Callback0,
+  f2Callback1,
+  f2Callback2,
+  f2Callback3,
+  f2Callback4,
+  f2Callback5,
+}) {
   return (
     <div id="2">
       <h1>2층</h1>
-      <div>
-        <p>처음 먹은 카드를 체크해주세요</p>
+      <div className="firstCard">
+        <p>처음 주운 카드를 체크해주세요</p>
         <button name="red" onClick={(e) => f2Callback0("red")}>
           레 드
         </button>
@@ -72,7 +79,316 @@ function F2({ f2Callback0, f2Callback1 }) {
         ></input>
         7
       </div>
-      <p>마지막 먹은 카드를 체크해주세요</p>
+
+      <div className="cards">
+        <p>각 결계를 해제하는데 사용한 카드의 번호를 체크해주세요</p>
+        레드{" "}
+        <input
+          type="radio"
+          name="redCard"
+          value="1"
+          onChange={(e) => f2Callback2(1, 0)}
+        ></input>
+        1
+        <input
+          type="radio"
+          name="redCard"
+          value="2"
+          onChange={(e) => f2Callback2(2, 0)}
+        ></input>
+        2
+        <input
+          type="radio"
+          name="redCard"
+          value="3"
+          onChange={(e) => f2Callback2(3, 0)}
+        ></input>
+        3
+        <input
+          type="radio"
+          name="redCard"
+          value="4"
+          onChange={(e) => f2Callback2(4, 0)}
+        ></input>
+        4
+        <input
+          type="radio"
+          name="redCard"
+          value="5"
+          onChange={(e) => f2Callback2(5, 0)}
+        ></input>
+        5
+        <input
+          type="radio"
+          name="redCard"
+          value="6"
+          onChange={(e) => f2Callback2(6, 0)}
+        ></input>
+        6
+        <input
+          type="radio"
+          name="redCard"
+          value="7"
+          onChange={(e) => f2Callback2(7, 0)}
+        ></input>
+        7
+        <input
+          type="radio"
+          name="redCard"
+          value="1"
+          onChange={(e) => f2Callback2(8, 0)}
+        ></input>
+        바이올렛 <br />
+        블루{" "}
+        <input
+          type="radio"
+          name="blueCard"
+          value="1"
+          onChange={(e) => f2Callback2(1, 1)}
+        ></input>
+        1
+        <input
+          type="radio"
+          name="blueCard"
+          value="2"
+          onChange={(e) => f2Callback2(2, 1)}
+        ></input>
+        2
+        <input
+          type="radio"
+          name="blueCard"
+          value="3"
+          onChange={(e) => f2Callback2(3, 1)}
+        ></input>
+        3
+        <input
+          type="radio"
+          name="blueCard"
+          value="4"
+          onChange={(e) => f2Callback2(4, 1)}
+        ></input>
+        4
+        <input
+          type="radio"
+          name="blueCard"
+          value="5"
+          onChange={(e) => f2Callback2(5, 1)}
+        ></input>
+        5
+        <input
+          type="radio"
+          name="blueCard"
+          value="6"
+          onChange={(e) => f2Callback2(6, 1)}
+        ></input>
+        6
+        <input
+          type="radio"
+          name="blueCard"
+          value="7"
+          onChange={(e) => f2Callback2(7, 1)}
+        ></input>
+        7
+        <input
+          type="radio"
+          name="blueCard"
+          value="7"
+          onChange={(e) => f2Callback2(8, 1)}
+        ></input>
+        바이올렛
+        <br />
+        그린{" "}
+        <input
+          type="radio"
+          name="greenCard"
+          value="1"
+          onChange={(e) => f2Callback2(1, 2)}
+        ></input>
+        1
+        <input
+          type="radio"
+          name="greenCard"
+          value="2"
+          onChange={(e) => f2Callback2(2, 2)}
+        ></input>
+        2
+        <input
+          type="radio"
+          name="greenCard"
+          value="3"
+          onChange={(e) => f2Callback2(3, 2)}
+        ></input>
+        3
+        <input
+          type="radio"
+          name="greenCard"
+          value="4"
+          onChange={(e) => f2Callback2(4, 2)}
+        ></input>
+        4
+        <input
+          type="radio"
+          name="greenCard"
+          value="5"
+          onChange={(e) => f2Callback2(5, 2)}
+        ></input>
+        5
+        <input
+          type="radio"
+          name="greenCard"
+          value="6"
+          onChange={(e) => f2Callback2(6, 2)}
+        ></input>
+        6
+        <input
+          type="radio"
+          name="greenCard"
+          value="7"
+          onChange={(e) => f2Callback2(7, 2)}
+        ></input>
+        7
+        <input
+          type="radio"
+          name="greenCard"
+          value="7"
+          onChange={(e) => f2Callback2(8, 2)}
+        ></input>
+        바이올렛
+        <br />
+        옐로우
+        <input
+          type="radio"
+          name="yellowCard"
+          value="1"
+          onChange={(e) => f2Callback2(1, 3)}
+        ></input>
+        1
+        <input
+          type="radio"
+          name="yellowCard"
+          value="2"
+          onChange={(e) => f2Callback2(2, 3)}
+        ></input>
+        2
+        <input
+          type="radio"
+          name="yellowCard"
+          value="3"
+          onChange={(e) => f2Callback2(3, 3)}
+        ></input>
+        3
+        <input
+          type="radio"
+          name="yellowCard"
+          value="4"
+          onChange={(e) => f2Callback2(4, 3)}
+        ></input>
+        4
+        <input
+          type="radio"
+          name="yellowCard"
+          value="5"
+          onChange={(e) => f2Callback2(5, 3)}
+        ></input>
+        5
+        <input
+          type="radio"
+          name="yellowCard"
+          value="6"
+          onChange={(e) => f2Callback2(6, 3)}
+        ></input>
+        6
+        <input
+          type="radio"
+          name="yellowCard"
+          value="7"
+          onChange={(e) => f2Callback2(7, 3)}
+        ></input>
+        7
+        <input
+          type="radio"
+          name="yellowCard"
+          value="7"
+          onChange={(e) => f2Callback2(8, 3)}
+        ></input>
+        바이올렛
+      </div>
+
+      <div className="lastCard">
+        <p>마지막 주운 카드를 체크해주세요</p>
+        <button name="red" onClick={(e) => f2Callback3("red")}>
+          레 드
+        </button>
+        <button name="blue" onClick={(e) => f2Callback3("blue")}>
+          블 루
+        </button>
+        <button name="green" onClick={(e) => f2Callback3("green")}>
+          그 린
+        </button>
+        <button name="yellow" onClick={(e) => f2Callback3("yellow")}>
+          옐로우
+        </button>
+        <button name="violet" onClick={(e) => f2Callback3("violet")}>
+          바이올렛
+        </button>
+        <br />
+        <input
+          type="radio"
+          name="lastCard"
+          value="1"
+          onChange={(e) => f2Callback4(1)}
+        ></input>
+        1
+        <input
+          type="radio"
+          name="lastCard"
+          value="2"
+          onChange={(e) => f2Callback4(2)}
+        ></input>
+        2
+        <input
+          type="radio"
+          name="lastCard"
+          value="3"
+          onChange={(e) => f2Callback4(3)}
+        ></input>
+        3
+        <input
+          type="radio"
+          name="lastCard"
+          value="4"
+          onChange={(e) => f2Callback4(4)}
+        ></input>
+        4
+        <input
+          type="radio"
+          name="lastCard"
+          value="5"
+          onChange={(e) => f2Callback4(5)}
+        ></input>
+        5
+        <input
+          type="radio"
+          name="lastCard"
+          value="6"
+          onChange={(e) => f2Callback4(6)}
+        ></input>
+        6
+        <input
+          type="radio"
+          name="lastCard"
+          value="7"
+          onChange={(e) => f2Callback4(7)}
+        ></input>
+        7
+      </div>
+      <div className="total">
+        <p>총 주운 카드 수를 입력해주세요</p>
+        <input
+          type="text"
+          onChange={(e) => f2Callback5(e.target.value)}
+        ></input>
+      </div>
     </div>
   );
 }
